a40739309b5c9bb1c3bf197a6716d8d6e99799a0
Kostya <kostikkan595@gmail.com>
2021-09-20 17:14:21 +0500

    feat: Added back neumorphism

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ for (let i = 0; i < burger.length; i++) {
         burger[i].classList.toggle('burger-active')
         burgerList[0].classList.toggle('burger_list-active')
 
-        
 
-        if(burger[i].classList.contains('burger-active')){
+
+        if (burger[i].classList.contains('burger-active')) {
             console.log('active');
             burgerLine[0].classList.add('upper_line-active')
             burgerLine[1].classList.add('middle_line-active')
             burgerLine[2].classList.add('lower_line-active')
-            
 
-        }else{
+
+        } else {
             burgerLine[0].classList.remove('upper_line-active')
             burgerLine[1].classList.remove('middle_line-active')
             burgerLine[2].classList.remove('lower_line-active')
 
         }
     })
-    
+
 }
+
+
+
